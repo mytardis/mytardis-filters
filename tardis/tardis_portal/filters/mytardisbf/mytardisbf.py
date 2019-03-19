2c0a1d4b0faa445f34f94aceb02cf5b3ecc763af
Dmitry Yakhnov <dmitry@yakhnov.info>
2019-03-19 17:09:07 +1100

    Sync

--- tardis/tardis_portal/filters/mytardisbf/mytardisbf.py
+++ tardis/tardis_portal/filters/mytardisbf/mytardisbf.py
@@ -2,6 +2,10 @@ import os
 import traceback
 import logging
 
+from xml.etree import ElementTree as et
+import numpy as np
+from scipy.ndimage import zoom
+
 import javabridge
 import bioformats
 from bioformats import log4j
@@ -9,7 +13,6 @@ from bioformats import log4j
 from django.conf import settings
 
 from ..helpers import fileFilter, get_thumbnail_paths
-from .metadata import get_meta
 
 logger = logging.getLogger(__name__)
 
@@ -33,6 +36,211 @@ def check_and_start_jvm():
             mtbf_jvm_started = True
         except javabridge.JVMNotFoundError as e:
             logger.debug(e)
+
+
+def get_namespaces(meta_xml_root):
+    """
+    Extract OME and StructuredAnnotation namespaces from OME-XML file
+
+    param meta_xml_root: Root ElementTree element
+    type meta_xml_root: element
+
+    return: Dictionary with the OME and SA namespaces
+    rtype: dict
+    """
+    ome_ns = meta_xml_root.tag[1:].split("}", 1)[0]
+    sa_ns = ome_ns.replace("OME", "SA")
+    return {'ome': ome_ns, 'sa': sa_ns}
+
+
+def get_meta(input_file_path, output_path, **kwargs):
+    """
+    Extract specific metadata typically used in bio-image analysis. Also
+    outputs a preview image to the output directory.
+
+    param input_file_path: Path to the input file
+    type input_file_path: string
+
+    param output_path: Path to the output file
+    type output_path: string
+
+    param kwargs: extra args
+    type kwargs: object
+
+    return: List of dicts containing with keys and values for specific metadata
+    rtype: dict
+
+    """
+    pix_exc = set(["id", "significantbits", "bigendian", "interleaved"])
+    channel_exc = set(["color", "id", "color", "contrastmethod", "fluor",
+                       "ndfilter", "illuminationtype", "name",
+                       "pockelcellsetting", "acquisitionmode"])
+
+    try:
+        omexml = bioformats.get_omexml_metadata(input_file_path).encode('utf-8')
+    except javabridge.jutil.JavaException:
+        logger.error("Unable to read OME Metadata from: %s", input_file_path)
+        return None
+
+    input_fname, _ = os.path.splitext(os.path.basename(input_file_path))
+
+    meta_xml = et.fromstring(omexml)
+    ome_ns = get_namespaces(meta_xml)
+    meta = list()
+    for i, img_meta in enumerate(meta_xml.findall('ome:Image', ome_ns)):
+        smeta = dict()
+        output_file_path = os.path.join(output_path,
+                                        input_fname + "_s%s.png" % i)
+        logger.debug("Generating series %s preview from image: %s",
+                     i, input_file_path)
+        img = get_preview_image(input_file_path, omexml, series=i)
+        logger.debug("Saving series %s preview from image: %s",
+                     i, input_file_path)
+        save_image(img, output_file_path, overwrite=True)
+        logger.debug("Extracting metadata for series %s preview from image: %s",
+                     i, input_file_path)
+        smeta['id'] = img_meta.attrib['ID']
+        smeta['name'] = img_meta.attrib['Name']
+        smeta['previewImage'] = output_file_path
+        for pix_meta in img_meta.findall('ome:Pixels', ome_ns):
+            for k, v in pix_meta.attrib.iteritems():
+                if k.lower() not in pix_exc:
+                    smeta[k.lower()] = v
+            for c, channel_meta in enumerate(
+                    pix_meta.findall('ome:Channel', ome_ns)):
+                for kc, vc in channel_meta.attrib.iteritems():
+                    if kc.lower() in channel_exc:
+                        continue
+                    if kc.lower() not in smeta:
+                        smeta[kc.lower()] = ["Channel %s: %s" % (c, vc)]
+                    else:
+                        smeta[kc.lower()].append("Channel %s: %s" % (c, vc))
+
+        meta.append(smeta)
+
+    return meta
+
+
+def stretch_contrast(img):
+    """Simple linear contrast stretch
+
+    Parameters
+    ----------
+    :param img: N x M array of grayscale image intensities
+    :type img: numpy.ndarray
+
+    Returns
+    -------
+    :return: Output image with contrast stretch over the 8-bit range.
+    :rtype: numpy.ndarray (dtype = np.int8)
+
+    """
+
+    s = np.subtract(img, np.min(img))
+    p = 255.0 / (np.max(img) - np.min(img))
+    out = np.round(np.multiply(s, p)).astype(np.uint8)
+
+    return out
+
+
+def get_preview_image(fname, meta_xml=None, maxwh=256, series=0):
+    """ Generate a thumbnail of an image at the specified path. Gets the
+    middle Z plane of channel=0 and timepoint=0 of the specified series.
+
+    Parameters
+    ----------
+    :param fname: Path to image file.
+    :type fname: string
+    :param meta_xml: OME XML metadata.
+    :type meta_xml: string
+    :param maxwh: Maximum width or height of the output thumbnail. If the
+        extracted image is larger in either x or y, the image will will be
+        downsized to fit.
+    :type maxwh: int
+    :param series: Series for multi-stack formats (default=0)
+    :type series: int
+
+    Returns
+    -------
+    :return: N x M array of grayscale pixel intentsities.
+    :rtype: numpy.ndarray
+
+    :raises Exception: bla
+
+    """
+    name, ext = os.path.splitext(os.path.basename(fname))
+
+    if ext[1:] not in bioformats.READABLE_FORMATS:
+        raise Exception("Format not supported: %s" % ext[1:])
+
+    if not meta_xml:
+        meta_xml = bioformats.get_omexml_metadata(fname)
+
+    ome = bioformats.OMEXML(meta_xml.decode('utf-8'))
+
+    if series > ome.get_image_count():
+        raise Exception("Specified series number %s exceeds number of series "
+                        "in the image file: %s" % (series, fname))
+
+    # Get metadata for first series
+    meta = ome.image(0).Pixels
+
+    # Determine resize factor
+    sizex = meta.SizeX
+    sizey = meta.SizeY
+    if sizex > maxwh or sizey > maxwh:
+        f = min(float(maxwh) / float(sizex), float(maxwh) / float(sizey))
+    else:
+        f = max(float(maxwh) / float(sizex), float(maxwh) / float(sizey))
+
+    # Determine which Z slice
+    z = 0
+
+    # Determine middle Z plane
+    # Note: SizeZ doesn't seem to be reliable. Might need to check BF.
+    # Stick with first slice for now.
+    # if meta.SizeZ > 1:
+    #     z = int(math.floor(float(meta.SizeZ)/2))
+
+    # Load image plane
+    if meta.channel_count == 1 and meta.SizeC == 4:
+        # RGB Image
+        img = bioformats.load_image(fname, t=0, z=z, series=series,
+                                    rescale=False)
+        return zoom(img, (f, f, 1))
+
+    # Grayscale, grab channel 0 only
+    img = bioformats.load_image(fname, c=0, t=0, z=z, series=series,
+                                rescale=False)
+    # if img.dtype in (np.uint16, np.uint32, np.int16, np.int32):
+    img = stretch_contrast(img)
+    return zoom(img, f)
+
+
+def save_image(img, output_path, overwrite=False):
+    """
+    Extract and save a preview image for an input image
+
+    param img: N x M array of grayscale pixel intentsities.
+    type img: ndarray
+
+    param output_path: Path to which the output file is to be saved.
+    type output_path: string
+
+    param overwrite: Specifies whether or not to overwrite an existing file if
+        a file already exist at the output path.
+    type overwrite: bool
+
+    raises Exception: bla
+    """
+    if os.path.exists(output_path):
+        if not overwrite:
+            raise Exception("Ouput file %s already exists and parameter"
+                            "overwrite=False" % output_path)
+        else:
+            os.remove(output_path)
+
+    bioformats.write_image(output_path, img, bioformats.PT_UINT8)
 
 
 class BioformatsFilter(fileFilter):
@@ -86,7 +294,10 @@ class BioformatsFilter(fileFilter):
 
             rsp = get_meta(filename, os.path.dirname(thumb_abs_path), **kwargs)
             if rsp is not None:
-                return self.filter_metadata(rsp[0])
+                metadata = []
+                for i in rsp:
+                    metadata.append(self.filter_metadata(i))
+                return metadata[0]
 
         except Exception as e:
             logger.error(str(e))
--- tardis/tardis_portal/tests/test_bioformats.py
+++ tardis/tardis_portal/tests/test_bioformats.py
@@ -12,7 +12,7 @@ class BioformatsFilterTestCase(TransactionTestCase):
         self.filter = helpers.get_filter_settings('Bioformats')
         self.callable = safe_import(self.filter)
 
-    def testThumbnail(self):
+    def atestSimple(self):
         # Create mockup variables
         fname = 'sample.nd2'
         id = helpers.get_datafile_id()
@@ -20,7 +20,7 @@ class BioformatsFilterTestCase(TransactionTestCase):
         filename = helpers.create_datafile(fname, dsn)
         uri = path.join(dsn, fname)
 
-        # Generate thumbnail
+        # Generate thumbnail and extract metadata
         results = self.callable(id, filename, uri)
 
         # Basic schema checks
@@ -40,6 +40,49 @@ class BioformatsFilterTestCase(TransactionTestCase):
         self.assertTrue(
             'dimensionorder' in results and results[
                 'dimensionorder'] == 'XYZCT')
+
+        # Cleanup
+        helpers.delete_datafile(uri)
+
+    def testMulti(self):
+        # Create mockup variables
+        fname = 'z-series.ome.tif'
+        id = helpers.get_datafile_id()
+        dsn = helpers.get_dataset_name()
+        filename = helpers.create_datafile(fname, dsn)
+        uri = path.join(dsn, fname)
+
+        # Generate thumbnails and extract metadata
+        results = self.callable(id, filename, uri)
+        # print(results)
+
+        # Basic schema checks
+        self.assertTrue(isinstance(results, dict))
+        self.assertTrue(
+            'previewImage' in results and len(results['previewImage']))
+
+        # Check that thumbnail file exists
+        self.assertTrue(
+            path.exists(helpers.get_thumbnail_file(results['previewImage'])))
+
+        # Extracted metadata check
+        self.assertTrue(
+            'name' in results and results['name'] == 'z-series.ome.tif')
+        self.assertTrue(
+            'id' in results and results['id'] == 'Image:0')
+        self.assertTrue(
+            'dimensionorder' in results and results[
+                'dimensionorder'] == 'XYCZT')
+        self.assertTrue(
+            'sizex' in results and results['sizex'] == '439')
+        self.assertTrue(
+            'sizey' in results and results['sizey'] == '167')
+        self.assertTrue(
+            'sizez' in results and results['sizez'] == '5')
+        self.assertTrue(
+            'sizec' in results and results['sizec'] == '1')
+        self.assertTrue(
+            'sizet' in results and results['sizet'] == '1')
 
         # Cleanup
         helpers.delete_datafile(uri)
